6944fe68e59037286d3f35cbdee46fc7e5c937d8
cmertz <chris@nimel.de>
2013-10-14 15:11:57 +0200

    extract list_devices() to own file

--- audiolevel.m
+++ audiolevel.m
@@ -50,34 +50,9 @@ getAudioRecorder()
     return recorder;
 }
 
-void
-listDevices()
-{
-    NSArray *devices = [AVCaptureDevice devices];
-
-    for (AVCaptureDevice *device in devices) {
-
-        NSLog(@"Device name: %@", [device localizedName]);
-
-        if ([device hasMediaType:AVMediaTypeVideo]) {
-
-            if ([device position] == AVCaptureDevicePositionBack) {
-                NSLog(@"Device position : back");
-            }
-            else {
-                NSLog(@"Device position : front");
-            }
-        }
-    }
-}
-
 int
 main(int argc, char const **argv)
 {
-#ifdef DEBUG
-    listDevices();
-#endif
-
     AVAudioRecorder *recorder = getAudioRecorder();
 
     while (true) {
