d06e2e0026b065e1f52aa15a9209185bb8507d86
Michael Strüder <mikezter@gmail.com>
2013-12-05 12:40:46 +0100

    add udpsocket conn to elastics

---
+++ rsock.rb
@@ -0,0 +1,10 @@
+require 'socket'
+
+socket = UDPSocket.new.tap{|s| s.connect('overmind-a.kaeuferportal.eu', 6666) }
+
+ARGF.each_line do |l|
+  puts l
+  stamp, avg, peak = *l.split(' ')
+  event = %Q({"@message":"soundlevel", "peak":#{peak}, "avg":#{avg}, "type":"soundlevel", "source":"rmokbox"})
+  socket.write(event)
+end
