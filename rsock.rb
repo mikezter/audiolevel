2a1d34e8bba76cff66dc75f9059d68a501f1e0dd
Michael Strüder <mikezter@gmail.com>
2013-12-10 14:08:17 +0100

    use hostname source

--- rsock.rb
+++ rsock.rb
@@ -2,10 +2,10 @@
 require 'socket'
 
 socket = UDPSocket.new.tap{|s| s.connect('overmind-a.kaeuferportal.eu', 6666) }
+source = `hostname`
 
 ARGF.each_line do |l|
-  puts l
   stamp, avg, peak = *l.split(' ')
-  event = %Q({"@message":"soundlevel", "peak":#{peak}, "avg":#{avg}, "type":"soundlevel", "source":"rmokbox"})
+  event = %Q({"@message":"soundlevel", "peak":#{peak}, "avg":#{avg}, "type":"soundlevel", "source":"#{source}"})
   socket.write(event)
 end
